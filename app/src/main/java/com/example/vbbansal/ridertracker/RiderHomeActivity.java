edc6247fd58909d1763031e2bc89853f95a28589
vipulbansal91 <vipulbansal91@gmail.com>
2018-02-25 08:29:19 +0530

    Adding ShredPreferences for Rider Home + Code Refactor
    (cherry picked from commit 517078e)

--- app/src/main/java/com/example/vbbansal/ridertracker/RiderHomeActivity.java
+++ app/src/main/java/com/example/vbbansal/ridertracker/RiderHomeActivity.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -24,19 +25,85 @@ import com.example.vbbansal.ridertracker.helper.ViewHelper;
 
 public class RiderHomeActivity extends AppCompatActivity {
 
-    private String senderAddress;
-    private int frequencyInMinute;
-    private AlarmManager alarmMgr;
-    private PendingIntent alarmIntent;
+    public final static String KEY_TRACKER_NAME = "trackerName";
+    public final static String KEY_TRACKER_NUMBER = "trackerNumber";
+    public final static String KEY_LOCATION_SHARING_FREQUENCY = "locationSharingFrequency";
+
     private final static int PICK_CONTACT_REQUEST_CODE = 1;
+
+    private String trackerName;
+    private String trackerNumber;
+    private int frequencyInMinute;
+    private boolean isLocationSharingOn;
+
+    private AlarmManager alarmMgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_home);
 
-        ViewHelper.disableComponent(findViewById(R.id.startLocationSharingButton));
-        ViewHelper.disableComponent(findViewById(R.id.stopLocationSharingButton));
+        alarmMgr = (AlarmManager) this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        trackerName = getSharedPreferences().getString(KEY_TRACKER_NAME, null);
+        trackerNumber = getSharedPreferences().getString(KEY_TRACKER_NUMBER, null);
+        frequencyInMinute = getSharedPreferences().getInt(KEY_LOCATION_SHARING_FREQUENCY, 30);
+        isLocationSharingOn = getNullableAlarmPendingIntent() != null;
+
+        //setup activity to previous state
+        setupActivity();
+    }
+
+    @Override
+    public void onPause() {
+        updateSharedPreferences();
+
+        super.onPause();
+    }
+
+    private void updateSharedPreferences() {
+        SharedPreferences.Editor editor = getSharedPreferences().edit();
+        editor.putString(KEY_TRACKER_NAME, trackerName);
+        editor.putString(KEY_TRACKER_NUMBER, trackerNumber);
+        editor.putInt(KEY_LOCATION_SHARING_FREQUENCY, frequencyInMinute);
+        editor.commit();
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return this.getPreferences(Context.MODE_PRIVATE);
+    }
+
+    public void startLocationSharing(View view) {
+        frequencyInMinute = Integer.parseInt(((TextView) findViewById(R.id.locationSharingFrequencyEditText)).getText().toString());
+        // Sender address has already been set in Contact Picker activity call-back !
+
+        shareLocation();
+    }
+
+    private void shareLocation() {
+        if (LocationHelper.isLocationPermissionAvailable(this) && SMSHelper.isSendSmsPermissionAvailable(this)) {
+            alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), frequencyInMinute * 60 *
+                    1000, getAlarmPendingIntent());
+
+            isLocationSharingOn = true;
+
+            setupActivity();
+
+        } else {
+            if (LocationHelper.isLocationPermissionAvailable(this) == false) {
+                LocationHelper.requestLocationPermission(this);
+            }
+
+            if (SMSHelper.isSendSmsPermissionAvailable(this) == false) {
+                SMSHelper.requestSendSmsPermission(this);
+            }
+            // later part of showing the rider Location is carried out by onRequestPermissionsResult
+        }
     }
 
     @Override
@@ -68,57 +135,26 @@ public class RiderHomeActivity extends AppCompatActivity {
         }
     }
 
-    public void startLocationSharing(View view) {
-        frequencyInMinute = Integer.parseInt(((TextView) findViewById(R.id.locationSharingFrequencyEditText)).getText().toString());
-        // Sender address has already been set in Contact Picker activity call-back !
-
-        shareLocation();
-    }
-
-    private void shareLocation() {
-        if (LocationHelper.isLocationPermissionAvailable(this) && SMSHelper.isSendSmsPermissionAvailable(this)) {
-            alarmMgr = (AlarmManager) this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-
-            Intent intent = new Intent(this, LocationSharingAlarm.class);
-            intent.putExtra(Constants.ADDRESS_TO_SHARE_LOCATION, senderAddress);
-            alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-
-            alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), frequencyInMinute * 60 * 1000, alarmIntent);
-
-            ((TextView) findViewById(R.id.riderHomeTextView1)).setText(
-                    getString(R.string.rider_home_start_location_share_on_text_view)
-            );
-
-            ViewHelper.disableComponent(findViewById(R.id.startLocationSharingButton));
-            ViewHelper.disableComponent(findViewById(R.id.locationSharingFrequencyEditText));
-            ViewHelper.disableComponent(findViewById(R.id.contactPickerButton));
-            ViewHelper.enableComponent(findViewById(R.id.stopLocationSharingButton));
-
-        } else {
-            if (LocationHelper.isLocationPermissionAvailable(this) == false) {
-                LocationHelper.requestLocationPermission(this);
-            }
-
-            if (SMSHelper.isSendSmsPermissionAvailable(this) == false) {
-                SMSHelper.requestSendSmsPermission(this);
-            }
-            // later part of showing the rider Location is carried out by onRequestPermissionsResult
-        }
+    private PendingIntent getAlarmPendingIntent() {
+        Intent intent = new Intent(this, LocationSharingAlarm.class);
+        intent.putExtra(Constants.ADDRESS_TO_SHARE_LOCATION, trackerNumber);
+        return PendingIntent.getBroadcast(this, 0, intent, 0);
     }
 
     public void stopLocationSharing(View view) {
-        if (alarmMgr != null) {
-            alarmMgr.cancel(alarmIntent);
+        if (getNullableAlarmPendingIntent() != null && alarmMgr != null) {
+            alarmMgr.cancel(getNullableAlarmPendingIntent());
+            getNullableAlarmPendingIntent().cancel();
 
-            ((TextView) findViewById(R.id.riderHomeTextView1)).setText(
-                    getString(R.string.rider_home_start_location_share_off_text_view)
-            );
-
-            ViewHelper.disableComponent(findViewById(R.id.stopLocationSharingButton));
-            ViewHelper.enableComponent(findViewById(R.id.startLocationSharingButton));
-            ViewHelper.enableComponent(findViewById(R.id.locationSharingFrequencyEditText));
-            ViewHelper.enableComponent(findViewById(R.id.contactPickerButton));
+            isLocationSharingOn = false;
+            setupActivity();
         }
+    }
+
+    private PendingIntent getNullableAlarmPendingIntent() {
+        Intent intent = new Intent(this, LocationSharingAlarm.class);
+        intent.putExtra(Constants.ADDRESS_TO_SHARE_LOCATION, trackerNumber);
+        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_NO_CREATE);
     }
 
     public void selectContact(View view) {
@@ -134,24 +170,92 @@ public class RiderHomeActivity extends AppCompatActivity {
             case PICK_CONTACT_REQUEST_CODE:
                 if (resultCode == Activity.RESULT_OK) {
                     Uri contactData = data.getData();
-                    Cursor c =  managedQuery(contactData, null, null, null, null);
+                    Cursor c = managedQuery(contactData, null, null, null, null);
                     if (c.moveToFirst()) {
                         String name = c.getString(c.getColumnIndexOrThrow(
                                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                         String number = c.getString(c.getColumnIndexOrThrow(
                                 ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-                        ((Button) findViewById(R.id.contactPickerButton)).setText(
-                                getString(R.string.rider_home_choose_another_contact));
+                        trackerNumber = number;
+                        trackerName = name;
 
-                        ((TextView)findViewById(R.id.chosenContactTextView)).setText(name + " or");
-                        findViewById(R.id.chosenContactTextView).setVisibility(View.VISIBLE);
-
-                        senderAddress = number;
-
-                        ViewHelper.enableComponent(findViewById(R.id.startLocationSharingButton));
+                        updateSharedPreferences();
+                        setupStartLocationSharingButton();
                     }
                 }
+
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+    private void setupActivity() {
+        setupTextView1();
+        setupChooseContactLayout();
+        setupLocationSharingFrequency();
+        setupStartLocationSharingButton();
+        setupStopLocationSharingButton();
+    }
+
+    private void setupTextView1() {
+        if (isLocationSharingOn) {
+            ((TextView) findViewById(R.id.riderHomeTextView1)).setText(
+                    getString(R.string.rider_home_start_location_share_on_text_view)
+            );
+        } else {
+            ((TextView) findViewById(R.id.riderHomeTextView1)).setText(
+                    getString(R.string.rider_home_start_location_share_off_text_view)
+            );
+        }
+    }
+
+    private void setupChooseContactLayout() {
+        if (trackerName != null) {
+            ((Button) findViewById(R.id.contactPickerButton)).setText(
+                    getString(R.string.rider_home_choose_another_contact));
+            ((TextView) findViewById(R.id.chosenContactTextView)).setText(trackerName + " or");
+
+            findViewById(R.id.chosenContactTextView).setVisibility(View.VISIBLE);
+        } else {
+            ((Button) findViewById(R.id.contactPickerButton)).setText(
+                    getString(R.string.rider_home_choose_contact));
+
+            findViewById(R.id.chosenContactTextView).setVisibility(View.INVISIBLE);
+        }
+
+        if (isLocationSharingOn) {
+            ViewHelper.disableComponent(findViewById(R.id.contactPickerButton));
+        } else {
+            ViewHelper.enableComponent(findViewById(R.id.contactPickerButton));
+        }
+
+    }
+
+    private void setupLocationSharingFrequency() {
+        ((TextView) findViewById(R.id.locationSharingFrequencyEditText)).setText(String.valueOf(frequencyInMinute));
+
+        if (isLocationSharingOn) {
+            ViewHelper.disableComponent(findViewById(R.id.locationSharingFrequencyEditText));
+        } else {
+            ViewHelper.enableComponent(findViewById(R.id.locationSharingFrequencyEditText));
+        }
+    }
+
+    private void setupStartLocationSharingButton() {
+        if (isLocationSharingOn == false && trackerName != null && frequencyInMinute > 0) {
+            ViewHelper.enableComponent(findViewById(R.id.startLocationSharingButton));
+        } else {
+            ViewHelper.disableComponent(findViewById(R.id.startLocationSharingButton));
+        }
+    }
+
+    private void setupStopLocationSharingButton() {
+        if (isLocationSharingOn) {
+            ViewHelper.enableComponent(findViewById(R.id.stopLocationSharingButton));
+        } else {
+            ViewHelper.disableComponent(findViewById(R.id.stopLocationSharingButton));
         }
     }
 }
